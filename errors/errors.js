ad2f3aa7b9bb0b67a9a346aa753761b922499e31
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2018-09-20 13:40:39 -0700

    WIP - errors

--- errors/errors.js
+++ errors/errors.js
@@ -13,8 +13,9 @@ const notFound = description => error(404, 'Not found', description);
 const internalServerError = description => error(500, 'Internal Server Error', description);
 
 const errorHandler = (res, err) => {
+  const message = 'The application encountered an unexpected condition.';
   console.error(err.stack);
-  res.status(500).send(internalServerError('The application encountered an unexpected condition.'));
+  res.status(500).send(internalServerError(message));
 };
 
 module.exports = {
