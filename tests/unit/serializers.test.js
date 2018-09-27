24c5b2392668f9ebf9ce4b2f50a5cfe69d5da46a
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2018-09-27 11:29:36 -0700

    Pagination - unit test

--- tests/unit/serializers.test.js
+++ tests/unit/serializers.test.js
@@ -3,6 +3,7 @@ const camelCase = require('camelcase');
 const chai = require('chai');
 const chaiString = require('chai-string');
 const _ = require('lodash');
+const url = require('url');
 
 const { resourceSerializer } = reqlib('/serializers/jsonapi');
 const { paginate } = reqlib('/serializers/paginator');
@@ -25,6 +26,15 @@ describe('Test paginator', () => {
     const page = { size: 10 };
     const { paginatedRows } = paginate(rows, page);
     assert.isAtMost(paginatedRows.length, page.size);
+    done();
+  });
+
+  it('should return default page number and size', (done) => {
+    const page = {};
+    const { paginationLinks } = paginate(rows, page);
+    const { query } = url.parse(paginationLinks.first, true);
+    assert.equal(query['page[number]'], 1);
+    assert.equal(query['page[size]'], 10);
     done();
   });
 });
