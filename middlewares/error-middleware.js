3daeb729ce86eb92d54dfb6c057acdba7e7411b8
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2018-12-05 09:51:57 -0800

    Add more comments

--- middlewares/error-middleware.js
+++ middlewares/error-middleware.js
@@ -3,9 +3,17 @@ const _ = require('lodash');
 
 const { errorBuilder, errorHandler } = appRoot.require('errors/errors');
 
+/**
+ * @summary The middleware for handling errors
+ */
 const errorMiddleware = (err, req, res, next) => { // eslint-disable-line no-unused-vars
   const { status, errors } = err;
 
+  /**
+   * express-openapi validates requests based on openapi specification. For example, if we specify
+   * the maximum of page[size] is 500 in openapi.yaml, then it'll return a 400 error if page[size]
+   * is exceeded the maximum. This logic is mainly to serialize the error by adhering to JSON API.
+   */
   if (status === 400) {
     const details = [];
     _.forEach(errors, (error) => {
