ede2bf768387455478bbbfab245afb9c1b78fc25
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2018-12-07 10:56:52 -0800

    Fix path

--- api/v1/db/oracledb/pets-dao-example.js
+++ api/v1/db/oracledb/pets-dao-example.js
@@ -2,8 +2,8 @@ const appRoot = require('app-root-path');
 const config = require('config');
 const _ = require('lodash');
 
-const contrib = appRoot.require('contrib/contrib');
-const { getConnection } = appRoot.require('db/connection');
+const contrib = appRoot.require('api/v1/db/oracledb/contrib/contrib');
+const { getConnection } = appRoot.require('api/v1/db/oracledb/connection');
 const { SerializedPets, SerializedPet } = require('../../serializers/pets-serializer');
 
 const { endpointUri } = config.get('server');
