c60e616b8a5b12b6cd1e369cb820e0aa1dcd1088
browni <ian.brown9475@gmail.com>
2019-04-02 16:12:46 -0700

    move api/v1/db/aws/ -> api/v1/db/awsS3/

--- utils/validate-data-source.js
+++ utils/validate-data-source.js
@@ -10,7 +10,7 @@ const oracledb = dataSources.includes('oracledb')
   ? appRoot.require('api/v1/db/oracledb/connection').validateOracleDb
   : null;
 const awsS3 = dataSources.includes('awsS3')
-  ? appRoot.require('api/v1/db/aws/aws-operations').validateAwsS3
+  ? appRoot.require('api/v1/db/awsS3/aws-operations').validateAwsS3
   : null;
 
 /**
