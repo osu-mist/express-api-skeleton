54acb4fce838b7b8031ffc158bdfeadb2a496693
ianbrown9475 <ian.brown9475@gmail.com>
2019-04-09 13:27:48 -0700

    Delete build/ directory before compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+const del = require('del');
 const forever = require('forever-monitor');
 const gulp = require('gulp');
 const babel = require('gulp-babel');
@@ -5,11 +6,15 @@ const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
 const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('babel', () => gulp.src(['**/*.js', '!build/**', '!node_modules/**'])
+gulp.task('babel-clean', () => del(['build']));
+
+gulp.task('babel-compile', () => gulp.src(['**/*.js', '!build/**', '!node_modules/**'])
   .pipe(sourcemaps.init())
   .pipe(babel({ presets: ['@babel/preset-env'] }))
   .pipe(sourcemaps.write('../maps'))
   .pipe(gulp.dest('build/dist')));
+
+gulp.task('babel', gulp.series('babel-clean', 'babel-compile'));
 
 /**
  * @summary Use Eslint linting *.js file besides source files in node_modules
