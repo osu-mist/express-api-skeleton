790841913f4d1b68eadf4eef92fda32741651646
ianbrown9475 <ian.brown9475@gmail.com>
2019-04-09 13:05:31 -0700

    Move generated code to build/ directory

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,16 @@ const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
 const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('babel', () => gulp.src(['**/*.js', '!dist/**', '!node_modules/**'])
+gulp.task('babel', () => gulp.src(['**/*.js', '!build/**', '!node_modules/**'])
   .pipe(sourcemaps.init())
   .pipe(babel({ presets: ['@babel/preset-env'] }))
   .pipe(sourcemaps.write('../maps'))
-  .pipe(gulp.dest('dist')));
+  .pipe(gulp.dest('build/dist')));
 
 /**
  * @summary Use Eslint linting *.js file besides source files in node_modules
  */
-gulp.task('lint', () => gulp.src(['**/*.js', '!dist/**', '!node_modules/**'])
+gulp.task('lint', () => gulp.src(['**/*.js', '!build/**', '!node_modules/**'])
   .pipe(eslint())
   .pipe(eslint.format())
   .pipe(eslint.failAfterError())
@@ -23,14 +23,14 @@ gulp.task('lint', () => gulp.src(['**/*.js', '!dist/**', '!node_modules/**'])
 /**
  * @summary Run unit test
  */
-gulp.task('test', () => gulp.src(['dist/tests/unit/*.js'])
+gulp.task('test', () => gulp.src(['build/dist/tests/unit/*.js'])
   .pipe(babel())
   .pipe(mocha({ reporter: 'spec' })));
 
 /**
  * @summary Start application using forever
  */
-gulp.task('start', () => new forever.Monitor('dist/app.js').start());
+gulp.task('start', () => new forever.Monitor('build/dist/app.js').start());
 
 
 /**
