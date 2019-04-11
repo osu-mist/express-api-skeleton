7bab9806b147ec7ad666b75f4093a457d5ddd4f6
ianbrown9475 <ian.brown9475@gmail.com>
2019-04-11 10:49:30 -0700

    Use gulp recommended task syntax, update precommit

--- gulpfile.js
+++ gulpfile.js
@@ -2,59 +2,66 @@ const { spawn } = require('child_process');
 const del = require('del');
 const forever = require('forever-monitor');
 const gulp = require('gulp');
-const babel = require('gulp-babel');
+const gulpBabel = require('gulp-babel');
 const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
 const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('babel-clean', () => del(['build']));
+const babelClean = () => del(['build']);
 
-gulp.task('babel-compile', () => gulp.src(['**/*.js', '!build/**', '!node_modules/**'])
+const babelCompile = () => gulp.src(['**/*.js', '!build/**', '!node_modules/**'])
   .pipe(sourcemaps.init())
-  .pipe(babel())
+  .pipe(gulpBabel())
   .pipe(sourcemaps.write('../maps'))
-  .pipe(gulp.dest('build/dist')));
+  .pipe(gulp.dest('build/dist'));
 
-gulp.task('babel', gulp.series('babel-clean', 'babel-compile'));
+const babel = gulp.series(babelClean, babelCompile);
 
 /**
  * @summary Use Eslint linting *.js file besides source files in node_modules
  */
-gulp.task('lint', () => gulp.src(['**/*.js', '!build/**', '!node_modules/**'])
+const lint = () => gulp.src(['**/*.js', '!build/**', '!node_modules/**'])
   .pipe(eslint())
   .pipe(eslint.format())
-  .pipe(eslint.failAfterError()));
+  .pipe(eslint.failAfterError());
 
 /**
  * @summary Check Flow types
  */
-gulp.task('typecheck', () => spawn('./node_modules/.bin/flow', ['check'], { stdio: 'inherit' }));
+const typecheck = () => spawn('./node_modules/.bin/flow', ['check'], { stdio: 'inherit' });
 
 /**
  * @summary Run unit tests
  */
-gulp.task('test', () => gulp.src(['build/dist/tests/unit/*.js'])
-  .pipe(mocha({ reporter: 'spec' })));
+const test = () => gulp.src(['build/dist/tests/unit/*.js'])
+  .pipe(mocha({ reporter: 'spec' }));
 
 /**
  * @summary Start application using forever
  */
-gulp.task('start', () => new forever.Monitor('build/dist/app.js').start());
-
+const start = () => new forever.Monitor('build/dist/app.js').start();
 
 /**
  * @summary Lint and compile, test, and start the application
  */
-exports.run = gulp.series(gulp.parallel('lint', 'typecheck', 'babel'), 'test', 'start');
+exports.run = gulp.series(gulp.parallel(lint, typecheck, babel), test, start);
 /**
  * @summary Compile and start the application only
  */
-exports.start = gulp.series('babel', 'start');
+exports.start = gulp.series(babel, start);
 /**
  * @summary Compile and test the application only
  */
-exports.test = gulp.series('babel', 'test');
+exports.test = gulp.series(babel, test);
 /**
  * @summary Compile the code only
  */
-exports.babel = babel();
+exports.babel = babel;
+/**
+ * @summary Lint the code only
+ */
+exports.lint = lint;
+/**
+ * @summary Typecheck the code only
+ */
+exports.typecheck = typecheck;
