94f1a9ac4127676ae9f858e0bfb269d31c52e02b
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2019-04-19 09:27:15 -0700

    Fix first link

--- utils/jsonapi.js
+++ utils/jsonapi.js
@@ -60,7 +60,7 @@ const serializerOptions = (serializerArgs) => {
     } = pagination;
 
     options.topLevelLinks = _.assign(options.topLevelLinks, {
-      first: paramsLink(resourceUrl, pageParamsBuilder(pageNumber, pageSize)),
+      first: paramsLink(resourceUrl, pageParamsBuilder(1, pageSize)),
       last: paramsLink(resourceUrl, pageParamsBuilder(totalPages, pageSize)),
       next: nextPage ? paramsLink(resourceUrl, pageParamsBuilder(nextPage, pageSize)) : null,
       prev: prevPage ? paramsLink(resourceUrl, pageParamsBuilder(prevPage, pageSize)) : null,
