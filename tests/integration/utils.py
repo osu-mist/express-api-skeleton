46bcf6f78993a798060405cd4a6e7c7e3f069e7e
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2019-05-21 14:17:34 -0700

    Fix W605

--- tests/integration/utils.py
+++ tests/integration/utils.py
@@ -237,7 +237,7 @@ def check_url(self, link_url, endpoint, query_params=None):
     base_url = self.base_url
     if self.local_test:
         """Local instances return self links without port and /api"""
-        base_url = re.sub(':\d{4}/api', '', self.base_url)
+        base_url = re.sub(r':\d{4}/api', '', self.base_url)
 
     link_url_obj = urllib.parse.urlparse(link_url)
     base_url_obj = urllib.parse.urlparse(base_url)
