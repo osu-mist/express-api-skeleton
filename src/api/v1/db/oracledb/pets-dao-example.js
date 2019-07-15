c6d1c0fbc2d14d73fd4c68ed075e85f49869bdca
Ian Wilson <wilsonia@oregonstate.edu>
2019-07-15 13:57:25 -0700

    use import/export

--- src/api/v1/db/oracledb/pets-dao-example.js
+++ src/api/v1/db/oracledb/pets-dao-example.js
@@ -1,7 +1,7 @@
 import config from 'config';
 import _ from 'lodash';
 
-import conn from './connection';
+import { getConnection } from './connection';
 import contrib from './contrib/contrib';
 
 import { serializePets, serializePet } from 'api/v1/serializers/pets-serializer';
@@ -14,7 +14,7 @@ const { endpointUri } = config.get('server');
  * @returns {Promise<Object[]>} Promise object represents a list of pets
  */
 const getPets = async () => {
-  const connection = await conn.getConnection();
+  const connection = await getConnection();
   try {
     const { rawPets } = await connection.execute(contrib.getPets());
     const serializedPets = serializePets(rawPets, endpointUri);
@@ -32,7 +32,7 @@ const getPets = async () => {
  *                            is not found
  */
 const getPetById = async (id) => {
-  const connection = await conn.getConnection();
+  const connection = await getConnection();
   try {
     const { rawPets } = await connection.execute(contrib.getPetById(id), id);
 
