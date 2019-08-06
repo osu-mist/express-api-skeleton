9e692ef182343189b60db67f8645d9ba63dc3d86
ianbrown9475 <ian.brown9475@gmail.com>
2019-08-06 15:30:35 -0700

    Refactor s3 stub, add rejected bucketExists case

--- tests/unit/aws-operations.test.js
+++ tests/unit/aws-operations.test.js
@@ -23,31 +23,36 @@ describe('Test aws-operations', () => {
     configGetStub = sinon.stub(config, 'get').withArgs('dataSources.awsS3').returns({});
   });
 
+  const createS3Stub = (method, stub) => {
+    const s3Stub = sinon.stub(AWS, 'S3').returns({
+      [method]: () => ({ promise: stub }),
+    });
+    awsOperations = proxyquire(`${appRoot}/api/v1/db/awsS3/aws-operations`, {
+      config: { get: configGetStub },
+      'aws-sdk': { S3: s3Stub },
+    });
+  };
+
   describe('bucketExists', () => {
     it('Should resolve as true if headBucket().promise resolves', () => {
       const headBucketPromiseStub = sinon.stub().resolves({});
-      const s3Stub = sinon.stub(AWS, 'S3').returns({
-        headBucket: () => ({ promise: headBucketPromiseStub }),
-      });
-      awsOperations = proxyquire(`${appRoot}/api/v1/db/awsS3/aws-operations`, {
-        config: { get: configGetStub },
-        'aws-sdk': { S3: s3Stub },
-      });
+      createS3Stub('headBucket', headBucketPromiseStub);
       const result = awsOperations.bucketExists();
       return result.should.eventually.be.fulfilled.and.equal(true);
     });
 
     it('Should resolve as false if headBucket().promise rejects with NotFound error code', () => {
       const headBucketPromiseStub = sinon.stub().rejects({ code: 'NotFound' });
-      const s3Stub = sinon.stub(AWS, 'S3').returns({
-        headBucket: () => ({ promise: headBucketPromiseStub }),
-      });
-      awsOperations = proxyquire(`${appRoot}/api/v1/db/awsS3/aws-operations`, {
-        config: { get: configGetStub },
-        'aws-sdk': { S3: s3Stub },
-      });
+      createS3Stub('headBucket', headBucketPromiseStub);
       const result = awsOperations.bucketExists();
       return result.should.eventually.be.fulfilled.and.equal(false);
+    });
+
+    it('Should reject if headBucket().promise rejects with unexpected error code', () => {
+      const headBucketPromiseStub = sinon.stub().rejects({ code: 'other' });
+      createS3Stub('headBucket', headBucketPromiseStub);
+      const result = awsOperations.bucketExists();
+      return result.should.be.rejected;
     });
   });
 });
