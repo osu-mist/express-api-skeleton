d07692dcc9e49b75ab7ac0d0677dab4126cc2dc9
ianbrown9475 <ian.brown9475@gmail.com>
2019-08-07 12:25:09 -0700

    Undo refactoring, add more assertions

--- tests/unit/aws-operations.test.js
+++ tests/unit/aws-operations.test.js
@@ -4,13 +4,14 @@ const chai = require('chai');
 const chaiExclude = require('chai-exclude');
 const chaiAsPromised = require('chai-as-promised');
 const config = require('config');
-const _ = require('lodash');
 const proxyquire = require('proxyquire');
 const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
 
 chai.should();
 chai.use(chaiExclude);
 chai.use(chaiAsPromised);
+chai.use(sinonChai);
 
 afterEach(() => {
   sinon.restore();
@@ -27,123 +28,122 @@ describe('Test aws-operations', () => {
       .returns({ bucket: testBucket });
   });
 
+  const getS3MethodStub = promiseStub => sinon.stub().returns(({ promise: promiseStub }));
+
   const createS3Stub = (stubs) => {
-    // aws-operations uses the .promise() method of s3 methods so this needs to be stubbed
-    const stubReturn = _.mapValues(stubs, value => (
-      () => ({ promise: value })
-    ));
-    const s3Stub = sinon.stub(AWS, 'S3').returns(stubReturn);
+    const s3Stub = sinon.stub(AWS, 'S3').returns(stubs);
     awsOperations = proxyquire(`${appRoot}/api/v1/db/awsS3/aws-operations`, {
       config: { get: configGetStub },
       'aws-sdk': { S3: s3Stub },
     });
   };
 
-  const handleTestCase = (stubs, method, assertion) => {
-    createS3Stub(stubs);
-    const result = method(awsOperations);
-    return assertion(result);
-  };
-
   describe('bucketExists', () => {
-    const testCases = [
-      {
-        description: 'Should resolve as true if headBucket().promise resolves',
-        headBucketStub: sinon.stub().resolves({}),
-        assertion: result => result.should.eventually.be.fulfilled.and.equal(true),
-      },
-      {
-        description: 'Should resolve as false if headBucket().promise rejects with NotFound error code',
-        headBucketStub: sinon.stub().rejects({ code: 'NotFound' }),
-        assertion: result => result.should.eventually.be.fulfilled.and.equal(false),
-      },
-      {
-        description: 'Should reject if headBucket().promise rejects with unexpected error code',
-        headBucketStub: sinon.stub().rejects({ code: 'other' }),
-        assertion: result => result.should.be.rejected,
-      },
-    ];
+    it('Should resolve as true if headBucket promise resolves', async () => {
+      const promiseStub = sinon.stub().resolves({});
+      const headBucketStub = getS3MethodStub(promiseStub);
+      createS3Stub({ headBucket: headBucketStub });
+      const result = awsOperations.bucketExists();
+      await result.should.eventually.be.fulfilled.and.equal(true);
+      headBucketStub.should.have.been.calledOnce;
+      promiseStub.should.have.been.calledOnce;
+    });
 
-    _.forEach(testCases, ({ description, headBucketStub, assertion }) => {
-      it(description, () => {
-        const stubs = { headBucket: headBucketStub };
-        const method = dao => dao.bucketExists();
-        return handleTestCase(stubs, method, assertion);
-      });
+    it('Should resolve as false if headBucket promise rejects with NotFound error', async () => {
+      const promiseStub = sinon.stub().rejects({ code: 'NotFound' });
+      const headBucketStub = getS3MethodStub(promiseStub);
+      createS3Stub({ headBucket: headBucketStub });
+      const result = awsOperations.bucketExists();
+      await result.should.eventually.be.fulfilled.and.equal(false);
+      headBucketStub.should.have.been.calledOnce;
+      promiseStub.should.have.been.calledOnce;
+    });
+
+    it('Should resolve as false if headBucket promise rejects with unexpected error', async () => {
+      const promiseStub = sinon.stub().rejects({ code: 'other' });
+      const headBucketStub = getS3MethodStub(promiseStub);
+      createS3Stub({ headBucket: headBucketStub });
+      const result = awsOperations.bucketExists();
+      await result.should.eventually.be.rejected;
+      headBucketStub.should.have.been.calledOnce;
+      promiseStub.should.have.been.calledOnce;
     });
   });
 
-  describe('validateAwsS3', () => {
-    const testCases = [
-      {
-        description: 'Should resolve if headBucket().promise resolves',
-        headBucketStub: sinon.stub().resolves({}),
-        assertion: result => result.should.eventually.be.fulfilled,
-      },
-      {
-        description: 'Should reject if headBucket().promise rejects',
-        headBucketStub: sinon.stub().rejects({ code: 'NotFound' }),
-        assertion: result => result.should.be.rejectedWith(Error, 'AWS bucket does not exist'),
-      },
-    ];
 
-    _.forEach(testCases, ({ description, headBucketStub, assertion }) => {
-      it(description, () => {
-        const stubs = { headBucket: headBucketStub };
-        const method = dao => dao.validateAwsS3();
-        return handleTestCase(stubs, method, assertion);
-      });
+  describe('validateAwsS3', () => {
+    it('Should resolve if headBucket promise resolves', async () => {
+      const promiseStub = sinon.stub().resolves({});
+      const headBucketStub = getS3MethodStub(promiseStub);
+      createS3Stub({ headBucket: headBucketStub });
+      const result = awsOperations.validateAwsS3();
+      await result.should.eventually.be.fulfilled;
+      headBucketStub.should.have.been.calledOnce;
+      promiseStub.should.have.been.calledOnce;
+    });
+
+    it('Should reject if headBucket().promise rejects', async () => {
+      const promiseStub = sinon.stub().rejects({ code: 'NotFound' });
+      const headBucketStub = getS3MethodStub(promiseStub);
+      createS3Stub({ headBucket: headBucketStub });
+      const result = awsOperations.validateAwsS3();
+      await result.should.eventually.be.rejectedWith(Error, 'AWS bucket does not exist');
+      headBucketStub.should.have.been.calledOnce;
+      promiseStub.should.have.been.calledOnce;
     });
   });
 
   describe('objectExists', () => {
-    const testCases = [
-      {
-        description: 'Should resolve as true if headObject().promise resolves',
-        headObjectStub: sinon.stub().resolves({}),
-        assertion: result => result.should.eventually.be.fulfilled.and.equal(true),
-      },
-      {
-        description: 'Should resolve as false if headObject().promise rejects with NotFound error code',
-        headObjectStub: sinon.stub().rejects({ code: 'NotFound' }),
-        assertion: result => result.should.eventually.be.fulfilled.and.equal(false),
-      },
-      {
-        description: 'Should reject if headObject().promise rejects with unexpected error code',
-        headObjectStub: sinon.stub().rejects({ code: 'other' }),
-        assertion: result => result.should.be.rejected,
-      },
-    ];
+    it('Should resolve as true if headObject promise resolves', async () => {
+      const promiseStub = sinon.stub().resolves({});
+      const headObjectStub = getS3MethodStub(promiseStub);
+      createS3Stub({ headObject: headObjectStub });
+      const result = awsOperations.objectExists('test-key');
+      await result.should.eventually.be.fulfilled.and.equal(true);
+      headObjectStub.should.have.been.calledOnce;
+      promiseStub.should.have.been.calledOnce;
+    });
 
-    _.forEach(testCases, ({ description, headObjectStub, assertion }) => {
-      it(description, () => {
-        const stubs = { headObject: headObjectStub };
-        const method = dao => dao.objectExists('test-key');
-        return handleTestCase(stubs, method, assertion);
-      });
+    it('Should resolve as false if headObject promise rejects with NotFound error', async () => {
+      const promiseStub = sinon.stub().rejects({ code: 'NotFound' });
+      const headObjectStub = getS3MethodStub(promiseStub);
+      createS3Stub({ headObject: headObjectStub });
+      const result = awsOperations.objectExists('test-key');
+      await result.should.eventually.be.fulfilled.and.equal(false);
+      headObjectStub.should.have.been.calledOnce;
+      promiseStub.should.have.been.calledOnce;
+    });
+
+    it('Should reject if headObject().promise rejects with unexpected error code', async () => {
+      const promiseStub = sinon.stub().rejects({ code: 'other' });
+      const headObjectStub = getS3MethodStub(promiseStub);
+      createS3Stub({ headObject: headObjectStub });
+      const result = awsOperations.objectExists('test-key');
+      await result.should.eventually.be.rejected;
+      headObjectStub.should.have.been.calledOnce;
+      promiseStub.should.have.been.calledOnce;
     });
   });
 
   describe('headObject', () => {
-    const testCases = [
-      {
-        description: 'Should resolve when headObject().promise resolves',
-        headObjectStub: sinon.stub().resolves({}),
-        assertion: result => result.should.eventually.be.fulfilled.and.deep.equal({}),
-      },
-      {
-        description: 'Should reject when headObject().promise rejects',
-        headObjectStub: sinon.stub().rejects({}),
-        assertion: result => result.should.be.rejectedWith({}),
-      },
-    ];
+    it('Should resolve when headObject promise resolves', async () => {
+      const promiseStub = sinon.stub().resolves({});
+      const headObjectStub = getS3MethodStub(promiseStub);
+      createS3Stub({ headObject: headObjectStub });
+      const result = awsOperations.headObject('test-key');
+      await result.should.eventually.be.fulfilled.and.deep.equal({});
+      headObjectStub.should.have.been.calledOnce;
+      promiseStub.should.have.been.calledOnce;
+    });
 
-    _.forEach(testCases, ({ description, headObjectStub, assertion }) => {
-      it(description, () => {
-        const stubs = { headObject: headObjectStub };
-        const method = dao => dao.headObject('test-key');
-        return handleTestCase(stubs, method, assertion);
-      });
+    it('Should reject when headObject().promise rejects', async () => {
+      const promiseStub = sinon.stub().rejects({});
+      const headObjectStub = getS3MethodStub(promiseStub);
+      createS3Stub({ headObject: headObjectStub });
+      const result = awsOperations.headObject('test-key');
+      await result.should.be.rejectedWith({});
+      headObjectStub.should.have.been.calledOnce;
+      promiseStub.should.have.been.calledOnce;
     });
   });
 });
