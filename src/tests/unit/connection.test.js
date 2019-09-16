adc2154b54040f98e6815e2b166fe84e462cbf01
ianbrown9475 <ian.brown9475@gmail.com>
2019-09-16 13:45:33 -0700

    Add getConnection test

---
+++ src/tests/unit/connection.test.js
@@ -0,0 +1,43 @@
+import chai from 'chai';
+import chaiExclude from 'chai-exclude';
+import chaiAsPromised from 'chai-as-promised';
+import config from 'config';
+import proxyquire from 'proxyquire';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+chai.should();
+chai.use(chaiExclude);
+chai.use(chaiAsPromised);
+chai.use(sinonChai);
+
+afterEach(() => { sinon.restore(); });
+
+describe('Test oracledb connection module', () => {
+  let configGetStub;
+  let connection;
+
+  beforeEach(() => {
+    configGetStub = sinon.stub(config, 'get')
+      .withArgs('dataSources.oracledb')
+      .returns({});
+  });
+
+  const createOracleDbStub = (stubs) => {
+    connection = proxyquire('api/v1/db/oracledb/connection', {
+      config: { get: configGetStub },
+      oracledb: stubs,
+    });
+  };
+
+  describe('getConnection', () => {
+    it('Should call createPool if pool is falsy', async () => {
+      const createPoolStub = sinon.stub()
+        .resolves({ getConnection: async () => 'test-connection' });
+      createOracleDbStub({ createPool: createPoolStub });
+      const result = connection.getConnection();
+      await result.should.eventually.be.fulfilled.and.deep.equal('test-connection');
+      createPoolStub.should.have.been.calledOnce.and.always.calledWithMatch({});
+    });
+  });
+});
