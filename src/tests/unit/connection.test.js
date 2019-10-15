36646384cb0a96639958876d1c0372b4aa0d025c
ianbrown9475 <ian.brown9475@gmail.com>
2019-10-15 09:07:59 -0700

    Add validateOracleDb tests

--- src/tests/unit/connection.test.js
+++ src/tests/unit/connection.test.js
@@ -27,6 +27,8 @@ describe('Test oracledb connection module', () => {
     connection = proxyquire('api/v1/db/oracledb/connection', {
       config: { get: configGetStub },
       oracledb: stubs,
+      // suppress logger output for testing
+      '../../../../utils/logger': { logger: { error: () => {} } },
     });
   };
 
@@ -40,6 +42,35 @@ describe('Test oracledb connection module', () => {
       const secondResult = connection.getConnection();
       await secondResult.should.eventually.be.fulfilled.and.deep.equal('test-connection');
       createPoolStub.should.have.been.calledOnce.and.always.calledWithMatch({});
+      createPoolStub.should.have.been.calledWithMatch({});
+    });
+  });
+
+  describe('validateOracleDb', () => {
+    it('Should resolve when connection.execute resolves', async () => {
+      const executeStub = sinon.stub().resolves();
+      const closeStub = sinon.stub().resolves();
+      const createPoolStub = sinon.stub()
+        .resolves({ getConnection: async () => ({ execute: executeStub, close: closeStub }) });
+      createOracleDbStub({ createPool: createPoolStub });
+      const result = connection.validateOracleDb();
+      await result.should.eventually.be.fulfilled;
+      createPoolStub.should.have.been.calledOnce;
+      executeStub.should.have.been.calledOnce;
+      closeStub.should.have.been.calledOnce;
+    });
+
+    it('Should reject when connection.execute rejects', async () => {
+      const executeStub = sinon.stub().rejects();
+      const closeStub = sinon.stub().resolves();
+      const createPoolStub = sinon.stub()
+        .resolves({ getConnection: async () => ({ execute: executeStub, close: closeStub }) });
+      createOracleDbStub({ createPool: createPoolStub });
+      const result = connection.validateOracleDb();
+      await result.should.be.rejected;
+      createPoolStub.should.have.been.calledOnce;
+      executeStub.should.have.been.calledOnce;
+      closeStub.should.have.been.calledOnce;
     });
   });
 });
