a30a63b3bae1c5758db20535eb038f9c7ae3a576
Alex Ruef <alex.ruef@oregonstate.edu>
2020-03-24 16:35:52 -0700

    Using lodash set function instead of my cool recursive function

--- src/utils/format-sub-objects.js
+++ src/utils/format-sub-objects.js
@@ -1,27 +1,6 @@
 import _ from 'lodash';
 
 /**
- * Recursively adds nesting sub-objects from keyList
- * Once keyList is empty finalValue is assigned to end the nesting objects
- *
- * @param {string[]} keyList
- * @param {object} currObject
- * @param {string} finalValue
- */
-const expandKey = (keyList, currObject, finalValue) => {
-  // if this is the last key in keyList set it equal to finalValue and end recursion
-  if (keyList.length === 1) {
-    currObject[keyList[0]] = finalValue;
-  } else {
-    // create empty object for next nesting level if it is null
-    if (!currObject[keyList[0]]) {
-      currObject[keyList[0]] = {};
-    }
-    expandKey(_.tail(keyList), currObject[keyList[0]], finalValue);
-  }
-};
-
-/**
  * Takes list of raw data from data source and looks for keys that should be expanded into
  * nested objects
  * { 'addressType.code': 'CM' } => { addressType: { code: 'CM' } }
@@ -33,7 +12,7 @@ const formatSubObjects = (rawData) => {
     _.forEach(data, (value, key) => {
       const splitKey = key.split('.');
       if (splitKey.length > 1) {
-        expandKey(splitKey, data, value);
+        _.set(data, splitKey, value);
       }
     });
   });
