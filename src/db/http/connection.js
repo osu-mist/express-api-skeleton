6fe6a911846162f382b4fcfd73452b22aeee480b
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2020-06-22 15:28:30 -0700

    Add http support

---
+++ src/db/http/connection.js
@@ -0,0 +1,27 @@
+import config from 'config';
+import rp from 'request-promise-native';
+
+import { logger } from 'utils/logger';
+
+const { baseUri, headers } = config.get('dataSources.http');
+
+const httpOptions = {
+  headers,
+  json: true,
+};
+
+/**
+ * Validate http connection and throw an error if invalid
+ *
+ * @returns {Promise} resolves if http connection can be established and rejects otherwise
+ */
+const validateHttp = async () => {
+  try {
+    await rp({ ...{ uri: baseUri }, ...httpOptions });
+  } catch (err) {
+    logger.error(err);
+    throw new Error('Unable to connect to HTTP data source');
+  }
+};
+
+export { httpOptions, validateHttp };
